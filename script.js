b882cc461a261bad543cfea19b9fb3d8bb503090
Victorwenney <251069272+Victorwenney@users.noreply.github.com>
2026-02-01 11:25:59 -0300

    Create the playRound function Create code to compare the results and increment to the variables
    Created the function playRound that has recieves the human choice and
    the computer choice and compares them to give the result of the round
    played.
    Created a code to recieve the result of the playRound function and use
    it to increment the score variable of the winned side.

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 
 function getComputerChoice(){
-    let computerChoice = Math.trunc((Math.random() * 100)) % 3;
-    if(computerChoice === 0){
-        return "Rock";
-    } else if(computerChoice === 1){
-        return "Paper";
+    let compChoice = Math.trunc((Math.random() * 100)) % 3;
+    if(compChoice === 0){
+        return "ROCK";
+    } else if(compChoice === 1){
+        return "PAPER";
     } else{
-        return "Scissors";
+        return "SCISSORS";
     }
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice(){
-    let humanChoice = prompt("Welcome to Rock-Paper-Scissors!\nWhat do you want to play?");
-    return humanChoice;
+    let hChoice = prompt("Welcome to Rock-Paper-Scissors!\nWhat do you want to play?");
+    hChoice = hChoice.toUpperCase();
+    return hChoice;
 }
-
-console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === "ROCK" && computerChoice === "SCISSORS"){
+        console.log("You win! " + humanChoice + " beats " + computerChoice +".");
+        return 1;
+    } else if(humanChoice === "PAPER" && computerChoice === "ROCK"){
+        console.log("You win! " + humanChoice + " beats " + computerChoice +".");
+        return 1;
+    } else if(humanChoice === "SCISSORS" && computerChoice === "PAPER"){
+        console.log("You win! " + humanChoice + " beats " + computerChoice +".");
+        return 1;
+    } else if(computerChoice === "ROCK" && humanChoice === "SCISSORS"){
+        console.log("You lose! " + computerChoice + " beats " + humanChoice +".");
+        return 2;
+    } else if(computerChoice === "PAPER" && humanChoice === "ROCK"){
+        console.log("You lose! " + computerChoice + " beats " + humanChoice +".");
+        return 2;
+    } else if(computerChoice === "SCISSORS" && humanChoice === "PAPER"){
+        console.log("You lose! " + computerChoice + " beats " + humanChoice +".");
+        return 2;
+    } else{
+        console.log("It's a draw! Both players played " + humanChoice +".");
+        return 3;
+    }
+}
+
+let result = 0;
+result = playRound(getHumanChoice(), getComputerChoice());
+switch (result){
+    case 1:
+        humanScore += 1;
+        break;
+    case 2:
+        computerScore += 1;
+        break;
+    default:
+        break;
+}
+console.log("Total score:\nUser: " + humanScore +"\nComputer: " + computerScore);
 
 
